40b42e482107e8b990c65aacbfb74bc24c974323
Alex Paul <alexpaul@users.noreply.github.com>
2018-01-19 18:44:37 -0500

    file cleanup

--- FindASpot/Services/YelpAPI.swift
+++ FindASpot/Services/YelpAPI.swift
@@ -15,24 +15,17 @@ class YelpAPI {
     static func searchVenue(placename: String, addressString: String?, coordinate: CLLocationCoordinate2D?,  completion: @escaping (Error?, [Place]?) -> Void) {
         
         var urlRequest: URLRequest!
-        
-        //var urlRequest = URLRequest(url: URL(string:"\(YelpAPIKeys.baseSearchURL)\(placename)&location=11375")!)
-        
-        // using an address
+                
+        // use address
         if let address = addressString { // e.g "Queens, NY"
             urlRequest = URLRequest(url: URL(string:"\(YelpAPIKeys.baseSearchURL)\(placename)&location=\(address)")!)
         }
         
-        // use coordinate
+        // else use coordinate
         else {
             let coordinate = CLLocationCoordinate2DMake(UserPreference.manager.getLatitude(), UserPreference.manager.getLongitude())
             urlRequest = URLRequest(url: URL(string:"\(YelpAPIKeys.baseSearchURL)\(placename)&latitude=\(coordinate.latitude)&longitude=\(coordinate.longitude)")!)
         }
-        
-        
-        // TODO: we could have an addressString or coordinate
-        // TODO: coordinate is user location coordinate
-        // TODO: update our location with coordinate
         
         urlRequest.setValue("Bearer \(YelpAPIKeys.apiKey)", forHTTPHeaderField: "Authorization")
         urlSession.dataTask(with: urlRequest) { (data, response, error) in
